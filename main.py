6e3dd8d1962cac83085f5d45dd387de6a52b457e
wherop <peter.t.winkler@gmail.com>
2024-12-02 19:59:49 +0100

    feat: make date arguments optional when running main.py. Assumes day from current date.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import argparse
 import importlib.util
 import sys
 from pathlib import Path
+from datetime import datetime
+import pytz
 from generate_day import create_day
 
 def load_day(year, day):
@@ -21,11 +23,38 @@ def load_day(year, day):
     spec.loader.exec_module(day)
     return day
 
+def get_est_now():
+    # Get the current time in UTC
+    utc_now = datetime.now(pytz.utc)
+
+    # Convert to EST (Eastern Standard Time)
+    est = pytz.timezone("US/Eastern")
+    return utc_now.astimezone(est)
+    
+
+def get_year():
+    est_now = get_est_now()
+
+    if est_now.month < 12:
+        year = est_now.year-1
+    else:
+        year = est_now.year
+    return year
+
+def get_day():
+    est_now = get_est_now()
+    
+    if est_now.month == 12 and est_now.day in range(1, 26):
+        return est_now.day
+    else:
+        return 1
+
+
 def main():
     # Set up argument parser
     parser = argparse.ArgumentParser(description="Run a specific day based on year and day.")
-    parser.add_argument("year", type=int, help="Year of the day (e.g., 2024)")
-    parser.add_argument("day", type=int, choices=range(1,26), help="Day of the challenge (1-25)")
+    parser.add_argument("--year", default=get_year(), type=int, help="Year of the day (e.g., 2024)")
+    parser.add_argument("--day", default=get_day(), type=int, choices=range(1,26), help="Day of the challenge (1-25)")
 
     # Parse arguments
     args = parser.parse_args()
